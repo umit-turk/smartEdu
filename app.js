20fc18c9ddff1b5fa53526f4849323a83eb8feb5
Umit Turk <uyt.turk@gmail.com>
2021-05-26 01:28:49 +0300

    partials, partials

--- app.js
+++ app.js
@@ -2,8 +2,23 @@ const express = require("express");
 
 const app = express();
 
+//Template Engine 
+app.set("view engine", "ejs");
+
+//Middlewears
+app.use(express.static("public"))//static dosyaları ğublic klosöründe olacak.
+
+//Routes
 app.get("/", (req, res) => {
-  res.send("index syfası");
+  res.status(200).render('index',{
+      page_name: "index"
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.status(200).render('about',{
+      page_name: "about"
+  });
 });
 
 const port = 3000;
