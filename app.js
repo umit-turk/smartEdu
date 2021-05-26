269bcd429f23730992f3cdada9e51722a0cb432f
Umit Turk <uyt.turk@gmail.com>
2021-05-27 00:39:20 +0300

    routes, controllers

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const pageRoute = require('./routes/pageRoute');
 
 const app = express();
 
@@ -9,17 +10,9 @@ app.set("view engine", "ejs");
 app.use(express.static("public"))//static dosyaları ğublic klosöründe olacak.
 
 //Routes
-app.get("/", (req, res) => {
-  res.status(200).render('index',{
-      page_name: "index"
-  });
-});
+app.use("/", pageRoute);// / ile ilgili istek geldiğinde bunu pageRote yönlendir.
 
-app.get("/about", (req, res) => {
-  res.status(200).render('about',{
-      page_name: "about"
-  });
-});
+
 
 const port = 3000;
 app.listen(port, () => {
