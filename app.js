f293dafcdf532cf78ce6ae232287304cc418be61
Umit Turk <uyt.turk@gmail.com>
2021-05-29 13:36:27 +0300

    course model

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const express = require("express");
+const mongoose = require('mongoose')
 const pageRoute = require('./routes/pageRoute');
+const courseRoute = require('./routes/courseRoute');
+const Course = require("./models/Course");
 
 const app = express();
+
+//Connect db
+await mongoose.connect('mongodb://localhost/smartedu-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+}).then(() => {
+  console.log('DB connected Successfuly')
+});
 
 //Template Engine 
 app.set("view engine", "ejs");
@@ -11,6 +24,7 @@ app.use(express.static("public"))//static dosyaları ğublic klosöründe olacak
 
 //Routes
 app.use("/", pageRoute);// / ile ilgili istek geldiğinde bunu pageRote yönlendir.
+app.use("/courses", courseRoute);
 
 
 
