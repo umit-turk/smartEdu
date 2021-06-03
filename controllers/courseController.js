2f4803eb14cc16868762b3127282ff94b9bc5052
Umit Turk <uyt.turk@gmail.com>
2021-06-04 01:03:06 +0300

    kurslari listelemek

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,13 +1,33 @@
 const Course = require("../models/Course");
 
 exports.createCourse = async (req, res) => {
-  const course = await Course.create(req.body);
+
   try {
+  const course = await Course.create(req.body);
+  
     res.status(201).json({
       status: "success",
       course,
     });
-  } catch {
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      error,
+    });
+  }
+};
+
+// Tüm kursları sıralama işlemi yapacağız
+exports.getAllCourses = async (req, res) => {
+
+  try {
+  const courses = await Course.find();
+  
+    res.status(200).render('courses', {//views 'ın içerisndeki courses.ejs yi render edeceğiz içerisine yakalamış olduğumuz coursesleri göndereceğiz.
+      courses,
+      page_name: 'courses',
+    })
+  } catch (error) {
     res.status(400).json({
       status: "fail",
       error,
