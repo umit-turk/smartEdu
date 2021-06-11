04babe13ae3e96636f7a77e49aecf3e9015684e5
Umit Turk <uyt.turk@gmail.com>
2021-06-12 01:27:52 +0300

    session

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require('mongoose')
+const session = require('express-session')
 const pageRoute = require('./routes/pageRoute');
 const courseRoute = require('./routes/courseRoute');
 const categoryRoute = require('./routes/categoryRoute');
@@ -21,12 +22,27 @@ const app = express();
 //Template Engine 
 app.set("view engine", "ejs");
 
+//Global Variable 
+global.userIN = null;
+
+
 //Middlewears
 app.use(express.static("public"))//static dosyaları ğublic klosöründe olacak.
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(session({
+  secret: 'my_keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}))
+
 
 //Routes
+app.use('*', (req, res, next) => {
+  userIN =req.session.userID;
+  next();
+})
+
 app.use("/", pageRoute);// / ile ilgili istek geldiğinde bunu pageRote yönlendir.
 app.use("/courses", courseRoute);
 app.use("/categories", categoryRoute);
