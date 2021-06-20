f86f5d1f31126da7f426bf0abcd45b6053eb1726
Umit Turk <uyt.turk@gmail.com>
2021-06-20 17:26:13 +0300

    search alanı

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -28,6 +28,7 @@ exports.getAllCourses = async (req, res) => {
 
 
   const categorySlug = req.query.categories; //query'den gelen parametreyi yakalayacak.
+  const query = req.query.search
 
   const category = await Category.findOne({slug:categorySlug})
 
@@ -37,7 +38,21 @@ exports.getAllCourses = async (req, res) => {
     filter = {category:category._id}
   }
 
-  const courses = await Course.find(filter).sort('-createdAt');
+  if(query){
+    filter = {name:query}
+  }
+
+  if(!query && !categorySlug){
+    filter.name = "",
+    filter.category = null
+  }
+
+  const courses = await Course.find({
+    $or:[
+      {name: {$regex: '.*' + filter.name + '.*', $options: 'i'}},
+      {category: filter.category}
+    ]
+  }).sort('-createdAt').populate('user');
 
   const categories = await Category.find()
   
@@ -60,11 +75,13 @@ exports.getCourse = async (req, res) => {
   try {
     const user = await User.findById(req.session.userID)
     const course = await Course.findOne({slug: req.params.slug}).populate('user')
-  
+    const categories = await Category.find()
+
     res.status(200).render('course', {//views 'ın içerisndeki courses.ejs yi render edeceğiz içerisine yakalamış olduğumuz coursesleri göndereceğiz.
       course,
       page_name: 'courses',
-      user
+      user,
+      categories
     })
   } catch (error) {
     res.status(400).json({
