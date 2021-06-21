dc89295e75e1c17d0be56d3de3b2366a3c57880e
Umit Turk <uyt.turk@gmail.com>
2021-06-21 12:59:08 +0300

    admin alani ve kullanici silmek

--- controllers/authController.js
+++ controllers/authController.js
@@ -68,10 +68,31 @@ exports.getDashboardPage = async (req, res) => {
   ); // Bu kullanıcıya eşit olan id'yi getir
   const categories = await Category.find();
   const courses = await Course.find({ user: req.session.userID });
+  const users = await User.find();
   res.status(200).render("dashboard", {
     page_name: "dashboard",
     user, // dashboard templetine gönderiyoruz.
     categories,
     courses,
+    users
   });
+};
+
+/////
+
+exports.deleteUser = async (req, res) => {
+  try {
+
+     await User.findByIdAndRemove(req.params.id)
+     await Course.deleteMany({user:req.params.id})
+
+
+    res.status(200).redirect('/users/dashboard');
+
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      error,
+    });
+  }
 };
